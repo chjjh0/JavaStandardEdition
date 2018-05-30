e87caf0876feb11efb7c60479d98e19b2f539fe4
gnssp <chjjh@naver.com>
2018-05-30 02:16:53 +0000

    d

---
+++ loop/WhileDemo.java
@@ -0,0 +1,21 @@
+public class WhileDemo {
+
+public static void main(String[] args){
+
+	int count = 1, sum = 0;
+	String result = "";
+    while(count < 11){
+        if(count == 10){
+            result += count + "=";
+        } else { 
+            result += count + "+";
+        }
+        sum += count;
+        if(count == 10){
+        result += sum + "\n";
+        }
+        count++;     
+    }
+    System.out.print(result);
+	}
+}
